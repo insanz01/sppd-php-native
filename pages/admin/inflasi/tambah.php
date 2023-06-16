1d84b2936e8cfd22c592ab0621d6127b50967027
insanz01 <insankamil002@gmail.com>
2023-06-16 09:21:54 +0700

    change inflasi

--- pages/admin/inflasi/tambah.php
+++ pages/admin/inflasi/tambah.php
@@ -6,12 +6,12 @@
   <div class="container-fluid">
     <div class="row mb-2">
       <div class="col-sm-6">
-        <h1 class="m-0">Data Permintaan</h1>
+        <h1 class="m-0">Data Inflasi</h1>
       </div><!-- /.col -->
       <div class="col-sm-6">
         <ol class="breadcrumb float-sm-right">
-          <li class="breadcrumb-item"><a href="#">Permintaan</a></li>
-          <li class="breadcrumb-item active">Tambah Permintaan</li>
+          <li class="breadcrumb-item"><a href="#">Inflasi</a></li>
+          <li class="breadcrumb-item active">Tambah Inflasi</li>
         </ol>
       </div><!-- /.col -->
     </div><!-- /.row -->
